eda9e16076fcfc09284e0d3cfe48f9d044e990b0
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-28 18:19:13 +0200

    Add an ftp core module

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs   = require ('fs');
+const path = require ('path');
+
+
+exports.get = function (urlObj, outputFile, callback, callbackProgress) {
+  const xFs    = require ('xcraft-core-fs');
+  const Client = require ('ftp');
+
+  xFs.mkdir (path.dirname (outputFile));
+
+  var progress = 0;
+
+  const file = fs.createWriteStream (outputFile);
+  const c    = new Client();
+
+  c.on ('ready', () => {
+    c.size (urlObj.pathname, (err, total) => {
+      if (err) {
+        callback (err);
+        return;
+      }
+
+      c.get (urlObj.pathname, (err, stream) => {
+        if (err) {
+          callback (err);
+          return;
+        }
+
+        stream.once ('close', function () {
+          c.end ();
+        });
+
+        stream
+        .on ('data', function (data) {
+          if (!callbackProgress) {
+            return;
+          }
+
+          progress += data.length;
+          callbackProgress (progress, total);
+        })
+        .on ('error', callback)
+        .pipe (file)
+        .on ('finish', function () {
+          /* HACK: see xHttp. */
+          const fd = fs.openSync (outputFile, 'r');
+          fs.closeSync (fd);
+
+          callback ();
+        });
+      });
+    });
+  });
+
+  c.connect ({
+    host: urlObj.hostname
+  });
+};
