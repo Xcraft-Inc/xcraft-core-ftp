589a5d2861692ef0f87f6f0e1ed9e683f8aae559
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-29 18:53:04 +0200

    Rewrite xFtp with watt
    It removes the callbacks hell.

--- index.js
+++ index.js
@@ -2,58 +2,56 @@
 
 const fs   = require ('fs');
 const path = require ('path');
+const watt = require ('watt');
 
+
+function download (ftp, urlObj, outputFile, callback, callbackProgress) {
+  let progress = 0;
+
+  const file = fs.createWriteStream (outputFile);
+
+  watt (function * (next) {
+    const total  = yield ftp.size (urlObj.pathname, next);
+    const stream = yield ftp.get (urlObj.pathname, next);
+
+    yield new Promise ((resolve, reject) => {
+      stream
+        .once ('close', function () {
+          ftp.end ();
+        })
+        .on ('data', function (data) {
+          if (!callbackProgress) {
+            return;
+          }
+
+          progress += data.length;
+          callbackProgress (progress, total);
+        })
+        .on ('error', reject)
+        .pipe (file)
+        .on ('finish', function () {
+          /* HACK: see xHttp. */
+          const fd = fs.openSync (outputFile, 'r');
+          fs.closeSync (fd);
+          resolve ();
+        });
+    });
+  }) (callback);
+}
 
 exports.get = function (urlObj, outputFile, callback, callbackProgress) {
-  const xFs    = require ('xcraft-core-fs');
-  const Client = require ('ftp');
+  const xFs = require ('xcraft-core-fs');
+  const Ftp = require ('ftp');
 
   xFs.mkdir (path.dirname (outputFile));
 
-  var progress = 0;
+  const ftp = new Ftp ();
 
-  const file = fs.createWriteStream (outputFile);
-  const c    = new Client();
-
-  c.on ('ready', () => {
-    c.size (urlObj.pathname, (err, total) => {
-      if (err) {
-        callback (err);
-        return;
-      }
-
-      c.get (urlObj.pathname, (err, stream) => {
-        if (err) {
-          callback (err);
-          return;
-        }
-
-        stream
-          .once ('close', function () {
-            c.end ();
-          })
-          .on ('data', function (data) {
-            if (!callbackProgress) {
-              return;
-            }
-
-            progress += data.length;
-            callbackProgress (progress, total);
-          })
-          .on ('error', callback)
-          .pipe (file)
-          .on ('finish', function () {
-            /* HACK: see xHttp. */
-            const fd = fs.openSync (outputFile, 'r');
-            fs.closeSync (fd);
-
-            callback ();
-          });
-      });
-    });
+  ftp.on ('ready', () => {
+    download (ftp, urlObj, outputFile, callback, callbackProgress);
   });
 
-  c.connect ({
+  ftp.connect ({
     host: urlObj.hostname
   });
 };
